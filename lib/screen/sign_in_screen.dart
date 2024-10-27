eec8bc3ef847bd3f9af48e4d27a12423601818ac
Thi1503 <thile15032003@gmail.com>
2024-10-27 08:19:46 +0700

    Signed-off-by: Thi1503 <thile15032003@gmail.com>

---
+++ lib/screen/sign_in_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: const Text(''),
+      ),
+    );
+  }
+}
